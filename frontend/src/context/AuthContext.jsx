c5b8c0adcdad07837078adde6a2bfcf12f5ee1de
juanse203 <jsrojas203@gmail.com>
2026-05-05 09:29:12 -0500

    perdon si me lo tiro

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -16,33 +16,33 @@ export function AuthProvider({ children }) {
     setLoading(false)
   }, [])
 
-  async function login(telefono, password) {
-    const data = await authService.login({ telefono, password })
-    localStorage.setItem('rc_token', data.token)
-    localStorage.setItem('rc_user',  JSON.stringify(data.user))
-    setToken(data.token)
-    setUser(data.user)
-    return data.user
-  }
+  // async function login(telefono, password) {
+  //   const data = await authService.login({ telefono, password })
+  //   localStorage.setItem('rc_token', data.token)
+  //   localStorage.setItem('rc_user',  JSON.stringify(data.user))
+  //   setToken(data.token)
+  //   setUser(data.user)
+  //   return data.user
+  // }
 
-  async function register(payload) {
-    const data = await authService.register(payload)
-    localStorage.setItem('rc_token', data.token)
-    localStorage.setItem('rc_user',  JSON.stringify(data.user))
-    setToken(data.token)
-    setUser(data.user)
-    return data.user
-  }
+  // async function register(payload) {
+  //   const data = await authService.register(payload)
+  //   localStorage.setItem('rc_token', data.token)
+  //   localStorage.setItem('rc_user',  JSON.stringify(data.user))
+  //   setToken(data.token)
+  //   setUser(data.user)
+  //   return data.user
+  // }
 
-  function logout() {
-    localStorage.removeItem('rc_token')
-    localStorage.removeItem('rc_user')
-    setToken(null)
-    setUser(null)
-  }
+  // function logout() {
+  //   localStorage.removeItem('rc_token')
+  //   localStorage.removeItem('rc_user')
+  //   setToken(null)
+  //   setUser(null)
+  // }
 
   return (
-    <AuthContext.Provider value={{ user, token, loading, login, register, logout }}>
+    <AuthContext.Provider value={{ user, token, loading}}>
       {!loading && children}
     </AuthContext.Provider>
   )
